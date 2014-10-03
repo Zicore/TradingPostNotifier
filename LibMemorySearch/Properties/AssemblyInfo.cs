d774cc7a5b78d1752a0cf0aa3a149532884cc474
Zicore <zicore@gmx.de>
2014-10-03 09:55:23 +0200

    Removed memory search library
    No longer used

--- LibMemorySearch/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("LibMemorySearch")]
-[assembly: AssemblyDescription("")]
-
-[assembly: Guid("8999e44d-382c-43e4-b982-8144cf42918e")]
